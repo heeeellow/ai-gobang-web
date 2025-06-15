88ef1d3010f1e0aa4c5d61809b5bc6018aa991a7
YZH <26613320808@qq.com>
2025-06-15 20:53:00 +0800

    update utils

--- gobang-server/utils/logger.h
+++ gobang-server/utils/logger.h
@@ -9,7 +9,16 @@ class Logger {
 public:
     enum Level { INFO, WARNING, ERROR };
 
-    static void log(Level level, const std::string& msg);
+    static void log(Level level, const std::string& msg) {
+    static std::mutex mtx;
+    std::lock_guard<std::mutex> lock(mtx);
+    const char* level_str[] = {"[INFO]", "[WARN]", "[ERROR]"};
+    std::time_t now = std::time(nullptr);
+    char buf[32];
+    std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+    std::cout << buf << " " << level_str[level] << " " << msg << std::endl;
+}
+
     static void info(const std::string& msg) { log(INFO, msg); }
     static void warn(const std::string& msg) { log(WARNING, msg); }
     static void error(const std::string& msg) { log(ERROR, msg); }
